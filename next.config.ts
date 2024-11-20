f8e2b56be07af5de55e71d2c34e6d429631a05bf
Forekast <Forekast@users.noreply.github.com>
2024-11-20 16:10:20 -0500

    Render image collection-ish

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/portfolio",
-  output: "export",  // <=== enables static exports
+  output: "export", // <=== enables static exports
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  }
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
---
+++ src/app/images.ts
@@ -0,0 +1,155 @@
+export interface ImageModel {
+  path: string;
+  name: string;
+  description: string;
+  alt: string;
+}
+
+export interface GalleryModel {
+  name: string;
+  description: string;
+  images: ImageModel[];
+}
+
+export interface CollectionModel {
+  name: string;
+  galleries: GalleryModel[];
+}
+
+export const Collections: CollectionModel[] = [
+  {
+    name: "Design",
+    galleries: [
+      {
+        name: "Template",
+        description: "",
+        images: [
+          {
+            path: "blank.png",
+            name: "Template Image",
+            description: "Template Description",
+            alt: "Template Alt Text",
+          },
+        ],
+      },
+      {
+        name: "Yelena's Manual Therapy",
+        description: "",
+        images: [
+          {
+            path: "yelenas.png",
+            name: "name text",
+            description: "",
+            alt: "alt text 1",
+          },
+        ],
+      },
+      {
+        name: "GearUp VAR",
+        description: "",
+        images: [
+          {
+            path: "gearupvar.png",
+            name: "name text",
+            description: "",
+            alt: "alt text 1",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Games & Art",
+    galleries: [
+      {
+        name: "co.llide",
+        description: "",
+        images: [
+          {
+            path: "collide.png",
+            name: "name text",
+            description: "",
+            alt: "alt text 1",
+          },
+          {
+            path: "factions.png",
+            name: "name text",
+            description: "",
+            alt: "alt text 1",
+          },
+          {
+            path: "mascot.png",
+            name: "name text",
+            description: "",
+            alt: "alt text 1",
+          },
+        ],
+      },
+      {
+        name: "Nyx",
+        description: "",
+        images: [
+          {
+            path: "nyx.png",
+            name: "name text",
+            description: "",
+            alt: "alt text 1",
+          },
+        ],
+      },
+      {
+        name: "Pinfall",
+        description: "",
+        images: [
+          {
+            path: "pinfall.png",
+            name: "name text",
+            description: "",
+            alt: "alt text 1",
+          },
+        ],
+      },
+      {
+        name: "TinkerTech",
+        description: "",
+        images: [
+          {
+            path: "tinkertech.png",
+            name: "name text",
+            description: "",
+            alt: "alt text 1",
+          },
+        ],
+      },
+      {
+        name: "Senior Project",
+        description: "",
+        images: [
+          {
+            path: "claire.png",
+            name: "name text",
+            description: "",
+            alt: "alt text 1",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Web Apps",
+    galleries: [
+      {
+        name: "Knowledge Management Tool",
+        description: "",
+        images: [
+          {
+            path: "ECA.png",
+            name: "name text",
+            description: "",
+            alt: "alt text 1",
+          },
+        ],
+      },
+    ],
+  },
+];
